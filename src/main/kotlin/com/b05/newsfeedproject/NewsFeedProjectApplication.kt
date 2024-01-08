de1b26cb58264ad485f394035ae471d93fced79b
LSW990918 <leesywon99@gmail.com>
2024-01-08 14:42:59 +0900

    처음 커밋

---
+++ src/main/kotlin/com/b05/newsfeedproject/NewsFeedProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.b05.newsfeedproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsFeedProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<NewsFeedProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/b05/newsfeedproject/NewsFeedProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.b05.newsfeedproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NewsFeedProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
