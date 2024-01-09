d0a9ce84204b4b791ace072d43452c6831163249
Mhun9799 <muhun1030@gmail.com>
2024-01-09 11:22:03 +0900

    feat : Controller 초기구조 설계, commentId 타입변경

--- src/main/kotlin/com/b05/newsfeedproject/domain/comments/dto/CommentResponse.kt
+++ src/main/kotlin/com/b05/newsfeedproject/domain/comments/dto/CommentResponse.kt
@@ -3,7 +3,7 @@ package com.b05.newsfeedproject.domain.comments.dto
 import kotlin.reflect.jvm.internal.impl.load.kotlin.AbstractBinaryClassAnnotationLoader.Companion
 
 data class CommentResponse(
-    val id: Long?,
+    val id: Int,
     val content: String,
     val date: String,
 
